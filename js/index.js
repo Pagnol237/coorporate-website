d74408cf4454139bcb3a77ebcca519e1402280d2
Pagnol237 <99295070+Pagnol237@users.noreply.github.com>
2022-03-31 00:31:05 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	var scrolllink = $('.scroll');
+	//smooth scrolling code
+
+	scrolllink.click(function(e){
+		e.preventDefault();
+		$('body,html').animate({
+			scrollTop:$(this.hash).offset().top
+		},1000);
+	});
+
+	$(window).scroll(function(){
+		var scroll = $(window).scrollTop();
+		if (scroll>30) {
+			$('.nav').addClass('nave');
+		}
+		else{
+			$('.nav').removeClass('nave');
+		}
+	});
+	$('#toggle_icon').click(function(){
+		$('ul').toggle(700);
+		
+	});
+
+
+});
